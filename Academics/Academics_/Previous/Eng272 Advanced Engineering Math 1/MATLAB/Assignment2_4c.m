aac97dbb3381ef33622b14fb4a8f5387056926d1
Bryan C Guner <bryan.guner@gmail.com>
2021-08-09 22:28:54 +0000

    update

--- Academics/Academics_/Previous/Eng272 Advanced Engineering Math 1/MATLAB/Assignment2_4c.m
+++
@@ -1,8 +0,0 @@
-t = 0:0.01:15;
-y = (-3.75) * exp(-t) + (0.75) * (0.75) * exp(-5 * t) + 2;
-plot(t, y, 'linewidth', 3)
-axis([0 10 -2.5 2.5])
-title('Problem 4 part c')
-xlabel('time')
-ylabel('Y(t)')
-grid
